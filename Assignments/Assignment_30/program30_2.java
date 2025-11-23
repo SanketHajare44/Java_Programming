f853c4ffb25f9f72803784282d6665db8cf7d0ff
SanketHajare44 <sankethajare44@gmail.com>
2025-11-23 23:10:42 +0530

    Add solution for Assignment_30 Q2

---
+++ Assignments/Assignment_30/program30_2.java
@@ -0,0 +1,76 @@
+/*
+    iRow : 4
+    iCol : 4
+
+            *	*	*	*	
+            *	*	*	#	
+            *	*	#	#	
+            *	#	#	#
+    
+    Diagonal pattern
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Required packages
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class pattern
+{   
+    
+    public void Display(int iRow, int iCol)
+    {   
+        if(iRow != iCol)
+        {
+           System.out.printf("Invalid input"); 
+           System.out.printf("Row number and column number should be same");
+           return; 
+        }
+        
+        int i = 0;
+        int j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {                
+            for(j = 1;j <= iCol; j++)
+            {
+                if(((i+j)==2 ) || ((i+j)==3 ) || ((i+j)==4 ) || ((i+j)==5 ))
+                {
+                    System.out.printf("*\t");
+                }
+                else
+                {
+                    System.out.printf("#\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}// End of pattern class
+
+
+class program30_2
+{
+    public static void main(String A[])
+    {   
+        int iValue1 = 0, iValue2 = 0;           // To accept user input
+        
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.print("Enter the number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+
+        pobj.Display(iValue1, iValue2);         // Method call
+        
+    }
+}// End of program30_2 class
