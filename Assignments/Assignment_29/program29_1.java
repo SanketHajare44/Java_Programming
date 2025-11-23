8a2fb7f64a311aa0b0b20bd80f3a27291e77a75f
SanketHajare44 <sankethajare44@gmail.com>
2025-11-23 19:34:20 +0530

    Add solution for Assignment_29 Q1

---
+++ Assignments/Assignment_29/program29_1.java
@@ -0,0 +1,68 @@
+/*  
+    Accept number from user and Display below pattern
+
+    Input1 : 4      Input2 : 4
+
+    Output :    1   2   3   4
+                5   6   7   8
+                9   1   2   3
+                4   5   6   7
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Required packages
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+        int iNo = 0;
+
+        for(i = 1,  iNo = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print(iNo+"\t");
+                iNo++;
+
+                if(iNo == 10)
+                {
+                    iNo = 1;
+                }
+
+            }
+            System.out.println();                      
+        }
+    }
+
+}// End of pattern class
+
+class program29_1
+{
+    public static void main(String A[])
+    {   
+        int iValue1 = 0;                                 // To accept user input1
+        int iValue2 = 0;                                 // To accept user input2
+        
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the number Rows: ");
+        iValue1 = sobj.nextInt();
+
+        System.out.print("Enter the number Columns: ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();                   // Method call
+
+        pobj.Display(iValue1, iValue2);
+        
+    }
+
+}// End of program29_1 class
