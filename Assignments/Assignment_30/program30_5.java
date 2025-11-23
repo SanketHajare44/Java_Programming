201ea164cb37934968574f003be16957a7088a48
SanketHajare44 <sankethajare44@gmail.com>
2025-11-23 23:12:01 +0530

    Add solution for Assignment_30 Q5

---
+++ Assignments/Assignment_30/program30_5.java
@@ -0,0 +1,82 @@
+/*
+    iRow : 4
+    iCol : 4
+
+            1	2	3	4	
+            1	*	*	4	
+            1	*	*	4	
+            1	2	3	4
+    
+    Diagonal pattern
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Required packages
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class pattern
+{   
+    
+    public void Display(int iRow, int iCol)
+    {   
+        
+        int i = 0;
+        int j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {                
+            for(j = 1;j <= iCol; j++)
+            {
+                if(i == 1)
+                {
+                    System.out.printf(j+"\t");
+                }
+                else if(i == iRow)
+                {
+                    System.out.printf(j+"\t");
+                }
+                else if(j == 1)
+                {
+                    System.out.printf(1+"\t");
+                }
+                else if(j == iCol)
+                {
+                    System.out.printf(j+"\t");
+                }
+                else
+                {
+                    System.out.printf("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}// End of pattern class
+
+
+class program30_5
+{
+    public static void main(String A[])
+    {   
+        int iValue1 = 0, iValue2 = 0;           // To accept user input
+        
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.print("Enter the number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+
+        pobj.Display(iValue1, iValue2);         // Method call
+        
+    }
+}// End of program30_5 class
