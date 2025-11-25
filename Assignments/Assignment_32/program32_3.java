73432b0d8fb979b217baff671e767a11018b4cbb
SanketHajare44 <sankethajare44@gmail.com>
2025-11-25 11:40:48 +0530

    Add solution for Assignment_32 Q3

---
+++ Assignments/Assignment_32/program32_3.java
@@ -0,0 +1,68 @@
+/*
+    iRow : 6
+    iCol : 6
+
+                *	*	*	*	*	*	
+                *	 	 	 	*	*	
+                *	 	 	*	 	*	
+                *	 	*	 	 	*	
+                *	*	 	 	 	*	
+                *	*	*	*	*	*
+
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Required packages
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class pattern
+{   
+    
+    public void Display(int iRow, int iCol)
+    {   
+        int i = 0, j = 0;
+
+        for (i = 1; i <= iRow; i++)
+        {
+            for (j = 1; j <= iCol; j++)
+            {
+                if (i == 1 || i == iRow || j == 1 || j == iCol || i + j == iCol + 1)
+                {
+                    System.out.print("*\t");
+
+                } else
+                {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}// End of pattern class
+
+class program32_3
+{
+    public static void main(String A[])
+    {   
+        int iValue1 = 0, iValue2 = 0;           // To accept user input
+        
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.print("Enter the number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.print("Enter the number of Columns : ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+
+        pobj.Display(iValue1, iValue2);         // Method call
+        
+    }
+}// End of program32_3 class
+
